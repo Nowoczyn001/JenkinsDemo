c8d7c94d2a23e0e1e6a88e64e4153b0a0b9e2fb5
Nowoczyn001 <70034794+Nowoczyn001@users.noreply.github.com>
2023-11-16 16:00:57 -0500

    Add files via upload

---
+++ Commission/src/test/java/TestCommission.java
@@ -0,0 +1,47 @@
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+@RunWith(JUnit4.class)
+public class TestCommission {
+    @Mock
+    private  Lock lock = Mockito.mock(Lock.class);
+
+    @Mock
+    private  Stock stock = Mockito.mock(Stock.class);
+
+    @Mock
+    private  Barrel barrel = Mockito.mock(Barrel.class);
+
+    @InjectMocks
+    CalculateCommission c =new CalculateCommission();
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetCommissionSkipWhileLoop() {
+    }
+
+    @Test
+    public void testGetCommissionEnterWhileLoop() {
+        when(lock.getCount()).thenReturn(5);
+        Lock l = spy(new Lock(5));
+        when(l.getCount()).thenReturn(5);
+        when(barrel.getCount()).thenReturn(5);
+        double com = c.getCommission(lock,stock,barrel);
+        Assert.assertEquals(520.0,com,0);
+    }
+}
